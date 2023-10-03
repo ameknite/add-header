f33f67773bcd59341917e8be13a97117675c901a
AmeKnite <104745335+ameknite@users.noreply.github.com>
2023-10-02 20:00:56 -0600

    extensions description

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ struct Args {
     #[argh(option, default = "PathBuf::from(\".\")")]
     dir: PathBuf,
 
-    /// extensions
+    /// extensions files, default to ".rs"
     #[argh(option, default = "String::from(\".rs\")")]
     extensions: String,
 }
