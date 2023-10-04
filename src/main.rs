0545a8d277fbd9f9c5982b53c5992bca066b3c6e
AmeKnite <104745335+ameknite@users.noreply.github.com>
2023-10-03 18:01:05 -0600

    add option to select comment style
    - read existing content in bytes instead of a String

--- src/main.rs
+++ src/main.rs
@@ -15,33 +15,38 @@ use walkdir::WalkDir;
 #[derive(FromArgs)]
 /// Add Header
 struct Args {
-    /// path to the header file, default to: ./header
-    #[argh(option, default = "PathBuf::from(\"./header\")")]
-    file: PathBuf,
+    /// path to the header file, default to: ./NOTICE
+    #[argh(option, default = "PathBuf::from(\"./NOTICE\")")]
+    header: PathBuf,
 
     /// directory to apply the header, default to current dir: .
     #[argh(option, default = "PathBuf::from(\".\")")]
     dir: PathBuf,
 
-    /// filter files by extensions, default to ".rs"
-    #[argh(option, default = "String::from(\".rs\")")]
+    /// select files by extension, default to "rs", format: "rs,js,kt"
+    #[argh(option, default = "String::from(\"rs\")")]
     extensions: String,
+
+    /// comment style, default to "//"
+    #[argh(option, default = "String::from(\"//\")")]
+    comment_style: String,
 }
 
 fn main() -> anyhow::Result<()> {
     let Args {
-        file,
+        header: file,
         dir,
         extensions,
+        comment_style,
     } = argh::from_env();
-    let header = get_header_content(&file)?;
+    let header = get_header_content(&file, &comment_style)?;
     insert_header(&dir, &header, &extensions)?;
     Ok(())
 }
 
-fn get_header_content(header_path: &Path) -> anyhow::Result<String> {
+fn get_header_content(header_path: &Path, comment_style: &str) -> anyhow::Result<String> {
     let mut header_file = File::open(header_path)
-        .context("Header file not found. Use the --file option or create a ./header file.")?;
+        .context("Header file not found. Use the --file option or create a ./NOTICE file.")?;
 
     let mut contents = String::new();
     header_file.read_to_string(&mut contents)?;
@@ -49,7 +54,7 @@ fn get_header_content(header_path: &Path) -> anyhow::Result<String> {
     // Add '//' to the beginning of each line
     let mut header_comment = contents
         .lines()
-        .map(|line| format!("// {line}\n"))
+        .map(|line| format!("{comment_style} {line}\n"))
         .collect();
 
     // Add a whole new line
@@ -68,18 +73,23 @@ fn insert_header(dir: &Path, header: &str, extensions: &str) -> anyhow::Result<(
             continue;
         };
 
-        // Skip if the file doesn't have the correct extension
-        if !extensions.contains(extension) {
+        if extensions.split(',').all(|e| e != extension) {
             continue;
         }
 
         // Read the existing content of the file
         let mut file = File::open(file_path)?;
-        let mut existing_content = String::new();
-        file.read_to_string(&mut existing_content)?;
+        let mut existing_content = Vec::new();
+        file.read_to_end(&mut existing_content)?;
+
+        // Convert header to bytes
+        let header_bytes = header.trim().as_bytes();
 
         // Skip if the content of the header already exist
-        if existing_content.contains(header.trim()) {
+        if existing_content
+            .windows(header_bytes.len())
+            .any(|window| window == header_bytes)
+        {
             continue;
         }
 
@@ -88,7 +98,7 @@ fn insert_header(dir: &Path, header: &str, extensions: &str) -> anyhow::Result<(
 
         // Write the header followed by the existing content
         new_file.write_all(header.as_bytes())?;
-        new_file.write_all(existing_content.as_bytes())?
+        new_file.write_all(&existing_content)?;
     }
     Ok(())
 }
